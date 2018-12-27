d2979d761d367f1c106c9d6b478cfb979608e1a6
Katarzyna Jóźwiak <kasjozw@gmail.com>
2018-12-27 12:55:49 +0100

    add: adding person to database

--- app/src/main/java/com/example/kasia/mobilefamily/AddFamilyMemberActivity.java
+++ app/src/main/java/com/example/kasia/mobilefamily/AddFamilyMemberActivity.java
@@ -1,7 +1,14 @@
 package com.example.kasia.mobilefamily;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddFamilyMemberActivity extends AppCompatActivity {
 
@@ -9,5 +16,37 @@ public class AddFamilyMemberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_family_member);
+    }
+
+
+    public void addFamilyMember(View view){
+        SQLiteOpenHelper familyDataBaseHelper = new FamilyDataBaseHelper(this);
+        SQLiteDatabase db  =familyDataBaseHelper.getReadableDatabase();
+
+        EditText nameText = findViewById(R.id.nameText);
+        EditText sureNameText = findViewById(R.id.surnameText);
+        EditText dateOfBirthText = findViewById(R.id.dateOfBirthText);
+        EditText dateOfDeathText = findViewById(R.id.dateOfDeathText);
+        EditText nameDayDateText = findViewById(R.id.nameDayDateText);
+        EditText phoneNumbeText = findViewById(R.id.phoneNumberText);
+        EditText emailText = findViewById(R.id.emailText);
+        EditText addressText = findViewById(R.id.addressText);
+        EditText weddingDateText = findViewById(R.id.weddingDateText);
+        EditText workPlaceText = findViewById(R.id.workPlaceText);
+
+        ContentValues familyMemberValues = new ContentValues();
+        familyMemberValues.put("name", nameText.getText().toString());
+        familyMemberValues.put("surname", sureNameText.getText().toString());
+        familyMemberValues.put("date_of_birth", dateOfBirthText.getText().toString());
+        familyMemberValues.put("date_of_death", dateOfDeathText.getText().toString());
+        familyMemberValues.put("date_of_name_day", nameDayDateText.getText().toString());
+        familyMemberValues.put("telephone_number", phoneNumbeText.getText().toString());
+        familyMemberValues.put("email", emailText.getText().toString());
+        familyMemberValues.put("address", addressText.getText().toString());
+        familyMemberValues.put("place_of_work", workPlaceText.getText().toString());
+        familyMemberValues.put("name", nameText.getText().toString());
+
+        db.insert("person", null, familyMemberValues);
+        Toast.makeText(this,"Dodano członka rodziny"+ nameText.getText().toString() ,Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/kasia/mobilefamily/ListFamilyMembersActivity.java
+++ app/src/main/java/com/example/kasia/mobilefamily/ListFamilyMembersActivity.java
@@ -1,32 +1,39 @@
 package com.example.kasia.mobilefamily;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListFamilyMembersActivity extends AppCompatActivity {
 
-    String[] familyMembers = new String[] {"Janina Kowalska", "Jan Kowalski", "Kazimierz Nowak", "Katarzyna Jozwiak"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_member_data);
 
+        SQLiteOpenHelper familyDataBaseHelper = new FamilyDataBaseHelper(this);
+        SQLiteDatabase db  =familyDataBaseHelper.getReadableDatabase();
+        //Cursor cursor = db.query("person", new String[] {"name", "surname"},null,null,null,null,null );
+        Cursor cursor = db.rawQuery("SELECT  * FROM person", null);
         ListView eventsListView =  findViewById(R.id.familyMembersList);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.family_member_item,R.id.name, familyMembers);
-        eventsListView.setAdapter(adapter);
-        eventsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                openFamilyMemberDetails(familyMembers[i]);
-            }
-        });
 
+        TodoCursorAdapter todoAdapter = new TodoCursorAdapter(this, cursor);
+
+        eventsListView.setAdapter(todoAdapter);
 
         Button addMemberButton = findViewById(R.id.button2);
         addMemberButton.setOnClickListener(new View.OnClickListener(){
@@ -37,7 +44,7 @@ public class ListFamilyMembersActivity extends AppCompatActivity {
             }
         });
 
-
+        todoAdapter.changeCursor(cursor);
 
     }
 
@@ -52,5 +59,28 @@ public class ListFamilyMembersActivity extends AppCompatActivity {
     public void openAddFamilyMemberActivity(){
         Intent intent = new Intent(this, AddFamilyMemberActivity.class);
         startActivity(intent);
+    }
+
+    public class TodoCursorAdapter extends CursorAdapter {
+        public TodoCursorAdapter(Context context, Cursor cursor) {
+            super(context, cursor, 0);
+        }
+
+        // The newView method is used to inflate a new view and return it,
+        // you don't bind any data to the view at this point.
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return LayoutInflater.from(context).inflate(R.layout.family_member_item, parent, false);
+        }
+
+        // The bindView method is used to bind all data to a given view
+        // such as setting the text on a TextView.
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            TextView nameText = view.findViewById(R.id.name);
+            String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+            String surname = cursor.getString(cursor.getColumnIndexOrThrow("surname"));
+            nameText.setText(name + " "+surname);
+        }
     }
 }
