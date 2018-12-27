a4d9d8d623437e5928f292256f942a475fae03a2
Katarzyna Jóźwiak <kasjozw@gmail.com>
2018-12-27 16:23:48 +0100

    add: display familymembers data

--- app/src/main/java/com/example/kasia/mobilefamily/ListFamilyMembersActivity.java
+++ app/src/main/java/com/example/kasia/mobilefamily/ListFamilyMembersActivity.java
@@ -2,11 +2,13 @@ package com.example.kasia.mobilefamily;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,25 +17,99 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class ListFamilyMembersActivity extends AppCompatActivity {
+    public Context context = this;
+    public ArrayList<Integer> ids = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_member_data);
 
-        SQLiteOpenHelper familyDataBaseHelper = new FamilyDataBaseHelper(this);
-        SQLiteDatabase db  =familyDataBaseHelper.getReadableDatabase();
-        //Cursor cursor = db.query("person", new String[] {"name", "surname"},null,null,null,null,null );
-        Cursor cursor = db.rawQuery("SELECT  * FROM person", null);
-        ListView eventsListView =  findViewById(R.id.familyMembersList);
 
-        TodoCursorAdapter todoAdapter = new TodoCursorAdapter(this, cursor);
+        //spinner (sorting value) handle
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sortingValues, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        eventsListView.setAdapter(todoAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                SQLiteOpenHelper familyDataBaseHelper = new FamilyDataBaseHelper(context);
+                SQLiteDatabase db  =familyDataBaseHelper.getReadableDatabase();
+
+                Resources res = getResources();
+                String[] items = res.getStringArray(R.array.sortingValues);
+                String sortByStr = items[position];
+                String sortBy ="";
+                if(sortByStr.equals("Imie")) {
+                    sortBy="name";
+                }
+                if(sortByStr.equals("Nazwisko")) {
+                    sortBy="surname";
+                }
+                if(sortByStr.equals("Data urodzenia")) {
+                    sortBy="date_of_birth";
+                }
+
+                Cursor cursor = db.rawQuery("SELECT  * FROM person ORDER BY " + sortBy, null);
+
+                ListView eventsListView =  findViewById(R.id.familyMembersList);
+
+                MyCursorAdapter myAdapter = new MyCursorAdapter(context, cursor);
+                eventsListView.setAdapter(myAdapter);
+
+                //make list od ids
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()) {
+                    ids.add(Integer.parseInt(cursor.getString(cursor.getColumnIndexOrThrow("_id")))); //add the item
+                    cursor.moveToNext();
+                }
+
+                eventsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int idx, long l) {
+                        openFamilyMemberDetails(ids.get(idx));
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                //database handle
+                SQLiteOpenHelper familyDataBaseHelper = new FamilyDataBaseHelper(context);
+                SQLiteDatabase db  =familyDataBaseHelper.getReadableDatabase();
+                Cursor cursor = db.rawQuery("SELECT  * FROM person", null);
+                ListView eventsListView =  findViewById(R.id.familyMembersList);
+
+                MyCursorAdapter myAdapter = new MyCursorAdapter(context, cursor);
+                eventsListView.setAdapter(myAdapter);
+
+                //make list od ids
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()) {
+                    ids.add(Integer.parseInt(cursor.getString(cursor.getColumnIndexOrThrow("_id")))); //add the item
+                    cursor.moveToNext();
+                }
+
+                eventsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int idx, long l) {
+                        openFamilyMemberDetails(ids.get(idx));
+                    }
+                });
+            }
+
+        });
+
 
         Button addMemberButton = findViewById(R.id.button2);
         addMemberButton.setOnClickListener(new View.OnClickListener(){
@@ -44,14 +120,14 @@ public class ListFamilyMembersActivity extends AppCompatActivity {
             }
         });
 
-        todoAdapter.changeCursor(cursor);
 
     }
 
-    public void openFamilyMemberDetails(String name){
+
+    public void openFamilyMemberDetails(Integer id){
         Intent intent = new Intent(this, MemberDetailsActivity.class);
         Bundle dataBundle = new Bundle();
-        dataBundle.putString("name", name);
+        dataBundle.putInt("id", id);
         intent.putExtras(dataBundle);
         startActivity(intent);
     }
@@ -61,8 +137,8 @@ public class ListFamilyMembersActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public class TodoCursorAdapter extends CursorAdapter {
-        public TodoCursorAdapter(Context context, Cursor cursor) {
+    public class MyCursorAdapter extends CursorAdapter {
+        public MyCursorAdapter(Context context, Cursor cursor) {
             super(context, cursor, 0);
         }
 
@@ -77,7 +153,7 @@ public class ListFamilyMembersActivity extends AppCompatActivity {
         // such as setting the text on a TextView.
         @Override
         public void bindView(View view, Context context, Cursor cursor) {
-            TextView nameText = view.findViewById(R.id.name);
+            TextView nameText = view.findViewById(R.id.nameTextView);
             String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
             String surname = cursor.getString(cursor.getColumnIndexOrThrow("surname"));
             nameText.setText(name + " "+surname);
--- app/src/main/java/com/example/kasia/mobilefamily/MemberDetailsActivity.java
+++ app/src/main/java/com/example/kasia/mobilefamily/MemberDetailsActivity.java
@@ -1,10 +1,15 @@
 package com.example.kasia.mobilefamily;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MemberDetailsActivity extends AppCompatActivity {
@@ -15,11 +20,40 @@ public class MemberDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_member_details);
 
         Bundle extras = getIntent().getExtras();
-        String name = extras.getString("name");
+        int id = extras.getInt("id");
 
-        TextView textView =  findViewById(R.id.name);
-        textView.setText(name);
 
+        SQLiteOpenHelper familyDataBaseHelper = new FamilyDataBaseHelper(this);
+        SQLiteDatabase db  =familyDataBaseHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT  * FROM person WHERE _id IS "+String.valueOf(id), null);
+
+        TextView name =  findViewById(R.id.nameTextView);
+        TextView birthdate =  findViewById(R.id.birthdateTextView);
+        TextView namedate =  findViewById(R.id.namedateTextView);
+        TextView phoneNumbeText = findViewById(R.id.phoneNumberTextView);
+        TextView emailText = findViewById(R.id.emailTextView);
+        TextView weddingDateText = findViewById(R.id.weddingDateTextView);
+        TextView addressText = findViewById(R.id.weddingDateTextView);
+        TextView workPlaceText = findViewById(R.id.workplaceTextView);
+        TextView dateOfDeathText = findViewById(R.id.dateOfDeathTextView);
+
+/*
+
+ telephone_number text not null , email text not null , address text not null , place_of_work text not null)";
+
+*/
+
+        if(cursor.moveToNext()) {
+            name.setText(cursor.getString(cursor.getColumnIndexOrThrow("name")) + " " + cursor.getString(cursor.getColumnIndexOrThrow("surname")));
+            birthdate.setText(cursor.getString(cursor.getColumnIndexOrThrow("date_of_birth")));
+            namedate.setText(cursor.getString(cursor.getColumnIndexOrThrow("date_of_name_day")));
+            phoneNumbeText.setText(cursor.getString(cursor.getColumnIndexOrThrow("telephone_number")));
+            emailText.setText(cursor.getString(cursor.getColumnIndexOrThrow("email")));
+            weddingDateText.setText("DOROBIC DO BAZY DANYCH");
+            addressText.setText(cursor.getString(cursor.getColumnIndexOrThrow("address")));
+            workPlaceText.setText(cursor.getString(cursor.getColumnIndexOrThrow("place_of_work")));
+            dateOfDeathText.setText(cursor.getString(cursor.getColumnIndexOrThrow("date_of_death")));
+        }
 
         Button addMemberButton =  findViewById(R.id.button3);
         addMemberButton.setOnClickListener(new View.OnClickListener(){
